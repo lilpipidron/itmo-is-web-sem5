37f6517bb2a7a95c445b896e7ca5dec4680172e5
kramskoy.vadim <kramskoy.vadim@wb.ru>
2024-11-02 02:13:22 +0300

    fix script

--- js/randomWinners.js
+++ js/randomWinners.js
@@ -1,29 +1,48 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    async function loadFileData(url) {
-        const response = await fetch(url);
-        const text = await response.text();
-        return text.trim().split("\n");
-    }
+  const n = 5;
 
-    const names = await loadFileData("data/names.txt");
-    const surnames = await loadFileData("data/surnames.txt");
-    const prizes = await loadFileData("data/prizes.txt");
+  async function loadData(file) {
+    const response = await fetch(file);
+    const text = await response.text();
+    return text.trim().split("\n");
+  }
 
-    const audio = new Audio("audio/gennadii-gorin-chihaet.mp3");
+  const names = await loadData("data/names");
+  const surnames = await loadData("data/surnames");
+  const prizes = await loadData("data/prizes");
+  const audio = new Audio("audio/gennadii-gorin-chihaet.mp3");
 
-    function addRandomWinner() {
-        const name = names[Math.floor(Math.random() * names.length)];
-        const surname = surnames[Math.floor(Math.random() * surnames.length)];
-        const prize = prizes[Math.floor(Math.random() * prizes.length)];
-        const date = new Date().toISOString().split("T")[0];
+  function addRandomWinner() {
+    const name = names[Math.floor(Math.random() * names.length)];
+    const surname = surnames[Math.floor(Math.random() * surnames.length)];
+    const prize = prizes[Math.floor(Math.random() * prizes.length)];
+    const date = new Date().toISOString().split("T")[0];
 
-        const winnerInfo = document.createElement('div');
-        winnerInfo.textContent = `${name} ${surname} выиграл(а) ${prize} на дату ${date}`;
+    const row = document.createElement("div");
+    row.classList.add("winners-grid-row");
 
-        document.getElementById('winnerList').appendChild(winnerInfo);
+    const nameCell = document.createElement("div");
+    nameCell.classList.add("winners-grid-cell");
+    nameCell.textContent = `${name} ${surname}`;
 
-        audio.play();
-    }
+    const prizeCell = document.createElement("div");
+    prizeCell.classList.add("winners-grid-cell");
+    prizeCell.textContent = prize;
 
-    addRandomWinner();
+    const dateCell = document.createElement("div");
+    dateCell.classList.add("winners-grid-cell");
+    dateCell.textContent = date;
+
+    row.appendChild(nameCell);
+    row.appendChild(prizeCell);
+    row.appendChild(dateCell);
+
+    document.querySelector(".winners-grid").appendChild(row);
+
+    const randomDelay = Math.floor(Math.random() * n + 1) * 1000;
+    audio.play();
+    setTimeout(addRandomWinner, randomDelay);
+  }
+
+  addRandomWinner();
 });
